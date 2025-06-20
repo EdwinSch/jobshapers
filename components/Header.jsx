9a968c2a1a684209eed83569ac5a6823d6040743
edwin <edwinschutjes@gmail.com>
2025-06-20 16:38:07 +0200

    add sticky header with bg-blur

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,9 +11,9 @@ const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <header className="w-full border-b border-slate-300 relative">
+    <header className="w-full border-b border-slate-300 sticky top-0 z-50">
       <ContactLint />
-      <div className="h-16 mx-auto max-w-7xl px-4 md:px-6 lg:px-8 flex justify-between items-center">
+      <div className="h-16 mx-auto max-w-7xl px-4 md:px-6 lg:px-8 flex justify-between items-center bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
         <div className="text-jsMidnight">LOGO</div>
         <Navigation />
         <AdminBtns />
