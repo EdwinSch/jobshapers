d319e34e1170288b71da4570b73c222698821236
edwin <edwinschutjes@gmail.com>
2025-07-28 12:32:02 +0200

    finish employee page

--- app/werknemers/page.jsx
+++ app/werknemers/page.jsx
@@ -1,5 +1,6 @@
 import EmployeeHero from "@/components/EmployeeHero";
 import SellingPointCard from "@/components/SellingPointCard";
+import TopJobs from "@/components/TopJobs";
 import { FaSuitcase, FaUserCheck, FaLink, FaRegCompass } from "react-icons/fa6";
 
 const EmployeesPage = () => {
@@ -45,6 +46,28 @@ const EmployeesPage = () => {
           />
         </section>
       </div>
+
+      {/* Info */}
+      <div className="bg-jsOffWhite">
+        <div className="mx-auto py-12 max-w-7xl px-4 md:px-6 lg:px-8">
+          <div className="mx-0 md:mx-20 p-6 md:p-10 bg-white border border-slate-300 rounded-md shadow-md flex flex-col items-center gap-8">
+            <h2 className="text-jsMidnight text-2xl md:text-3xl font-bold text-center">
+              Jouw carrière, onze missie.
+            </h2>
+            <hr className="border-2 border-jsGreen w-16" />
+            <p className="text-center text-jsText">
+              Bij JOBSHAPERS sta jij centraal. Of je nu net begint, een volgende
+              stap wilt zetten, of toe bent aan een nieuwe uitdaging: wij helpen
+              je om een baan te vinden die écht bij je past. Niet zomaar een
+              functie, maar een plek waar jij je talenten kunt inzetten en
+              verder kunt ontwikkelen.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Top vacatures */}
+      <TopJobs />
     </>
   );
 };
