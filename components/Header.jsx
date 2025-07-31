bcab7e8b6f4148e997d83d680d3161b6efa9abd9
edwin <edwinschutjes@gmail.com>
2025-07-31 12:03:13 +0200

    convert header logo into home link

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,6 +7,7 @@ import MobileMenuButton from "./MobileMenuButton";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
 import logo from "@/assets/js-logo.png";
+import Link from "next/link";
 import { useState } from "react";
 
 const Header = () => {
@@ -16,7 +17,9 @@ const Header = () => {
     <header className="w-full border-b border-slate-300 sticky top-0 z-50 bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
       <ContactLint />
       <div className="h-16 mx-auto max-w-7xl px-4 md:px-6 lg:px-8 flex justify-between items-center">
-        <Image src={logo} alt="logo" width={150} height={0} priority />
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width={150} height={0} priority />
+        </Link>
         <Navigation />
         <AdminBtns />
         <MobileMenuButton
